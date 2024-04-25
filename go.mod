55fa5db21985ecf426dba136c2f6b614c7e98640
Sohel Ahmed Jony <sohelahmedjony@gmail.com>
2024-04-25 09:19:47 +0600

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JonyBepary/plunder_chain
+
+go 1.21.5
---
+++ plunder.go
@@ -0,0 +1,27 @@
+package plunder_chain
+
+import "fmt"
+
+func Greeting() {
+	// cool message in color and coloured ascii art
+	fmt.Println("Hello, welcome to PlunderChain!")
+	fmt.Println("This is a blockchain implementation in Go.")
+	fmt.Println("This is a project by the PlunderChain team.")
+	fmt.Println("We are a group of developers who are passionate about blockchain technology.")
+	fmt.Println("We are excited to share our project with you.")
+	fmt.Println("We hope you enjoy it!")
+	fmt.Println("Let's get started!")
+	fmt.Println(" ")
+	// plunder ascii art
+	fmt.Println("  _______ _                 _     _     _ ")
+	fmt.Println(" |__   __| |               | |   | |   | |")
+	fmt.Println("    | |  | |__   __ _ _ __ | |__ | |__ | |")
+	fmt.Println("    | |  | '_ \\ / _` | '_ \\| '_ \\| '_ \\| |")
+	fmt.Println("    | |  | | | | (_| | |_) | | | | | | |_|")
+	fmt.Println("    |_|  |_| |_|\\__,_| .__/|_| |_|_| |_|(_)")
+	fmt.Println("                     | |")
+	fmt.Println("                     |_|")
+	fmt.Println(" ")
+	fmt.Println(" ")
+
+}
